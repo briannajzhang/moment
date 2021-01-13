40ad1b839aa830aa768d0c64d9970a50e5c29731
bzhang33 <bjzhang2@illinois.edu>
2021-01-12 20:46:50 -0800

    successfully set up login with JWT token

--- api/helpers/generate-new-jwt-token.js
+++ api/helpers/generate-new-jwt-token.js
@@ -26,22 +26,17 @@ module.exports = {
   },
 
 
-  fn: async function (inputs, exits) {
+  fn: async function (inputs) {
     // TODO
     const payload = {
       sub: inputs.subject, // subject
-      iss: "LogRocket Sails API" // issuer
+      iss: "NOTE IT Web Application" // issuer
     };
 
     const secret = sails.config.jwtSecret || process.env.JWT_SECRET;
     const token = jwt.sign(payload, secret, { expiresIn: "1d" });
 
     return token; 
-
-
-
   }
-
-
 };
 
